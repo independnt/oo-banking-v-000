8c94d69081e6b6cefa48337ae3ba672143a8c2a6
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-20 21:50:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,12 @@ def valid?
   if sender.valid? && receiver.valid?
 end
 
+def do_nothing
+end
+
 def execute_transaction
+  if @status == "complete"
+
   if self.valid?
     receiver.deposit(amount)
     sender.deduct(amount)
